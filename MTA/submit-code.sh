14bee152bbe210f6a9225e1b89b31ad289e8356b
Henry Eleonu <eleonuhenry@yahoo.com>
2022-10-10 09:25:28 +0100

    some changes to spark pod

--- MTA/submit-code.sh
+++ MTA/submit-code.sh
@@ -1,24 +1,24 @@
-kubectl exec -it spark-pod bash
-psql -U postgres -c \"create database mta_data;\"
-psql -U postgres -c \"\c mta_data;\""
-echo "switching bash sessions..."
-docker exec -ti spark-standalone_spark-master_1 bash -c "echo unzipping data...
-cd /opt/spark-data
-unzip -o MTA_2014_08-10.zip
-cd /opt/spark
-echo finished unzipping...
-/opt/spark/bin/spark-submit --master spark://spark-master:7077 \
---jars /opt/spark-apps/postgresql-42.2.22.jar \
---driver-memory 1G \
---executor-memory 1G \
-/opt/spark-apps/main.py"
+#kubectl exec -it spark-pod bash
+#psql -U postgres -c \"create database mta_data;\"
+#psql -U postgres -c \"\c mta_data;\""
+#echo "switching bash sessions..."
+#docker exec -ti spark-standalone_spark-master_1 bash -c "echo unzipping data...
+#cd /opt/spark-data
+#unzip -o MTA_2014_08-10.zip
+#cd /opt/spark
+#echo finished unzipping...
+#/opt/spark/bin/spark-submit --master spark://spark-master:7077 \
+#--jars /opt/spark-apps/postgresql-42.2.22.jar \
+#--driver-memory 1G \
+#--executor-memory 1G \
+#/opt/spark-apps/main.py"
 
 /opt/spark/bin/spark-submit \
-    --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+    --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark-sa \
     --master k8s://https://kubernetes.docker.internal:6443 \
     --deploy-mode cluster \
     --name spark-pi \
-    --class org.apache.spark.examples.SparkPi \
     --conf spark.executor.instances=5 \
-    --conf spark.kubernetes.container.image=<spark-image> \
-    local:///path/to/examples.jar
+    --conf spark.kubernetes.container.image=heleonu/spark-pod:1 \
+    local:///opt/spark/examples/src/main/python/pi.py
+    # --class org.apache.spark.examples.SparkPi \
